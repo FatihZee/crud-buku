65948192ba3160363c236fbc04deee256d97b1ac
FATIH FIKRY OKTAVIANTO <viananto1234@gmail.com>
2025-02-21 23:02:00 +0700

    HALOO

--- config/db.config.js
+++ config/db.config.js
@@ -3,7 +3,9 @@ require('dotenv').config();
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT, // Tambahkan ini
   dialect: 'mysql',
+  logging: false, // Matikan logging SQL agar lebih rapi
 });
 
 module.exports = sequelize;
